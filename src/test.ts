6ee531dd8542601ce43a6581f01346c09af2c257
zhazhengyin <touchrank@qq.com>
2024-07-05 16:24:19 +0800

    添加一组测试布局数据

--- src/test.ts
+++ src/test.ts
@@ -57,14 +57,97 @@
 // ]
 
 export const testData = [
-  {x: 0, y: 0, w: 2, h: 2, i: "0"},
-  {x: 2, y: 0, w: 2, h: 4, i: "1"},
-  {x: 4, y: 0, w: 2, h: 5, i: "2"},
-  {x: 6, y: 0, w: 2, h: 3, i: "3"},
-  {x: 8, y: 0, w: 2, h: 3, i: "4"},
-  {x: 10, y: 0, w: 2, h: 3, i: "5"},
-  {x: 0, y: 5, w: 2, h: 5, i: "6"},
-  {x: 2, y: 5, w: 2, h: 5, i: "7"},
-  {x: 4, y: 5, w: 2, h: 5, i: "8"},
-  {x: 5, y: 10, w: 4, h: 3, i: "9"}
+  {sortIndex: 0, x: 0, y: 0, w: 2, h: 2, i: "0"},
+  {sortIndex: 0, x: 2, y: 0, w: 2, h: 2, i: "1"},
+  {sortIndex: 0, x: 4, y: 0, w: 2, h: 2, i: "2"},
+  {sortIndex: 0, x: 6, y: 0, w: 2, h: 3, i: "3"},
+  {sortIndex: 0, x: 8, y: 0, w: 2, h: 2, i: "4"},
+  {sortIndex: 0, x: 10, y: 0, w: 2, h: 3, i: "5"},
+  {sortIndex: 0, x: 0, y: 5, w: 2, h: 2, i: "6"},
+  {sortIndex: 0, x: 2, y: 5, w: 2, h: 2, i: "7"},
+  {sortIndex: 0, x: 4, y: 5, w: 2, h: 2, i: "8"},
+  {sortIndex: 0, x: 5, y: 10, w: 4, h: 3, i: "9"}
+]
+
+export const testData2 = [
+  {
+    sortIndex: 0,
+    x: 0,
+    y: 0,
+    w: 1,
+    h: 20,
+    i: "0"
+  },
+  {
+    sortIndex: 1,
+    x: 1,
+    y: 0,
+    w: 1,
+    h: 25,
+    i: "1"
+  },
+  {
+    sortIndex: 2,
+    x: 2,
+    y: 0,
+    w: 2,
+    h: 9,
+    i: "2"
+  },
+  {
+    sortIndex: 3,
+    x: 4,
+    y: 0,
+    w: 2,
+    h: 7,
+    i: "3"
+  },
+  {
+    sortIndex: 4,
+    x: 6,
+    y: 0,
+    w: 2,
+    h: 5,
+    i: "4"
+  },
+  {
+    sortIndex: 7,
+    x: 6,
+    y: 5,
+    w: 2,
+    h: 10,
+    i: "5"
+  },
+  {
+    sortIndex: 5,
+    x: 8,
+    y: 0,
+    w: 2,
+    h: 8,
+    i: "6"
+  },
+  {
+    sortIndex: 9,
+    x: 2,
+    y: 9,
+    w: 2,
+    h: 11,
+    i: "7"
+  },
+  {
+    sortIndex: 6,
+    x: 10,
+    y: 0,
+    w: 2,
+    h: 8,
+    i: "8"
+  },
+  {
+    sortIndex: 8,
+    x: 8,
+    y: 8,
+    w: 2,
+    h: 10,
+    i: "9"
+  }
 ]
